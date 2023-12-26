6897cf8c25465d2d57ef405daf600ac1dfe0e9f5
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2023-12-26 09:42:53 +0530

    learning classList

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const btnsCloseModal = document.querySelector('.close-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnsCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
